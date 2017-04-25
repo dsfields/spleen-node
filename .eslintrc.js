1b960386a5a70fd27e03c5f76f6f010fc7cadc07
Daniel Fields <daniel.s.fields@gmail.com>
2017-04-25 01:02:23 -0400

    Tokenizer

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  "extends": "airbnb",
+  "plugins": [],
+  "rules": {
+    "arrow-body-style": 0,
+    "curly": 0,
+    "func-names": 0,
+    "max-len": ["error", 80],
+    "no-continue": 0,
+    "no-param-reassign": 0,
+    "no-plusplus": 0,
+    "no-restricted-syntax": ["warn", "ForStatement"],
+    "no-undef": 0,
+    "no-underscore-dangle": 0,
+    "no-shadow": 0,
+    "padded-blocks": 0,
+    "prefer-spread": 0,
+    "prefer-template": 0,
+    "strict": 0,
+    "valid-typeof": 0,
+  }
+};
