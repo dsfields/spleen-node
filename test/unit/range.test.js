9aeb3b74b6630a80c164a4251967ad7252e7dbe1
Daniel Fields <daniel.s.fields@gmail.com>
2017-06-28 23:27:22 -0400

    - Adding support to Clause to stringify null values. - Adding support to Range.between() to understand null values. - Adding stringify null value support to Range.

--- test/unit/range.test.js
+++ test/unit/range.test.js
@@ -8,17 +8,38 @@ const Range = require('../../lib/range');
 describe('Range', () => {
 
   describe('#constructor', () => {
-    it('should throw if lower is not string or number', () => {
+    it('should throw if lower is not string, number, or null', () => {
       assert.throws(() => {
         const range = new Range(false, 42);
         assert.isNotOk(range);
       }, TypeError);
     });
 
-    it('should throw if upper is not string or number', () => {
+    it('should throw if upper is not string, number, or null', () => {
       assert.throws(() => {
         const range = Range(24, {});
         assert.isNotOk(range);
+      }, TypeError);
+    });
+
+    it('should accept number for lower and upper', () => {
+      assert.doesNotThrow(() => {
+        const range = new Range(3.14, 42);
+        assert.isOk(range);
+      }, TypeError);
+    });
+
+    it('should accept string for lower and upper', () => {
+      assert.doesNotThrow(() => {
+        const range = new Range('bar', 'foo');
+        assert.isOk(range);
+      }, TypeError);
+    });
+
+    it('should accept null for lower and upper', () => {
+      assert.doesNotThrow(() => {
+        const range = new Range(null, null);
+        assert.isOk(range);
       }, TypeError);
     });
 
@@ -37,11 +58,32 @@ describe('Range', () => {
 
 
   describe('#between', () => {
-    it('should throw if value not string or number', () => {
+    it('should throw if value not string, number, or null', () => {
       assert.throws(() => {
         const range = new Range(24, 42);
         range.between({});
       });
+    });
+
+    it('should accept number', () => {
+      assert.doesNotThrow(() => {
+        const range = new Range(24, 42);
+        range.between(33);
+      }, TypeError);
+    });
+
+    it('should accept string', () => {
+      assert.doesNotThrow(() => {
+        const range = new Range(24, 42);
+        range.between('foo');
+      }, TypeError);
+    });
+
+    it('should accept null', () => {
+      assert.doesNotThrow(() => {
+        const range = new Range(24, 42);
+        range.between(null);
+      }, TypeError);
     });
 
     it('should be true if string between', () => {
@@ -71,6 +113,12 @@ describe('Range', () => {
     it('should be false if string greater than upper', () => {
       const range = new Range('b', 'y');
       const result = range.between('z');
+      assert.isFalse(result);
+    });
+
+    it('should be false if null not between strings', () => {
+      const range = new Range('b', 'y');
+      const result = range.between(null);
       assert.isFalse(result);
     });
 
@@ -104,9 +152,39 @@ describe('Range', () => {
       assert.isFalse(result);
     });
 
-    it('should be false if value not defined', () => {
+    it('should be false if null not between numerics', () => {
       const range = new Range(24, 42);
       const result = range.between(null);
+      assert.isFalse(result);
+    });
+
+    it('should be true if numeric between null and numeric', () => {
+      const range = new Range(null, 42);
+      const result = range.between(10);
+      assert.isTrue(result);
+    });
+
+    it('should be true if null between null and numeric', () => {
+      const range = new Range(null, 42);
+      const result = range.between(null);
+      assert.isTrue(result);
+    });
+
+    it('should be false if string compared to null', () => {
+      const range = new Range(null, 'z');
+      const result = range.between('a');
+      assert.isFalse(result);
+    });
+
+    it('should be true if null compared to string', () => {
+      const range = new Range(null, 'z');
+      const result = range.between(null);
+      assert.isFalse(result);
+    });
+
+    it('should be false if numeric not between null and numeric', () => {
+      const range = new Range(null, 42);
+      const result = range.between(100);
       assert.isFalse(result);
     });
   });
@@ -124,7 +202,7 @@ describe('Range', () => {
       assert.strictEqual(str, '"bar","foo"');
     });
 
-    it('should return stringified when mixed', () => {
+    it('should return stringified when number and string', () => {
       const range = new Range(24, 'foo');
       const str = range.toString();
       assert.strictEqual(str, '24,"foo"');
@@ -136,10 +214,46 @@ describe('Range', () => {
       assert.strictEqual(str, '"%3Ffoo","bar%26"');
     });
 
-    it('should return stringified string when URL encoding mixed', () => {
+    it('should return stringified when URL encoding number and string', () => {
       const range = new Range(24, '?foo');
       const str = range.toString(true);
       assert.strictEqual(str, '24,"%3Ffoo"');
+    });
+
+    it('should return stringified when nulls', () => {
+      const range = new Range(null, null);
+      const str = range.toString();
+      assert.strictEqual(str, 'nil,nil');
+    });
+
+    it('should return stringified when URL encoding with nulls', () => {
+      const range = new Range(null, null);
+      const str = range.toString(true);
+      assert.strictEqual(str, 'nil,nil');
+    });
+
+    it('should return stringified when null and string', () => {
+      const range = new Range(null, '?foo');
+      const str = range.toString();
+      assert.strictEqual(str, 'nil,"?foo"');
+    });
+
+    it('should return stringified when URL encoding null and string', () => {
+      const range = new Range(null, '?foo');
+      const str = range.toString(true);
+      assert.strictEqual(str, 'nil,"%3Ffoo"');
+    });
+
+    it('should return stringified when null and number', () => {
+      const range = new Range(null, 42);
+      const str = range.toString(true);
+      assert.strictEqual(str, 'nil,42');
+    });
+
+    it('should return stringified when URL encoding null and number', () => {
+      const range = new Range(null, 42);
+      const str = range.toString(true);
+      assert.strictEqual(str, 'nil,42');
     });
   });
 });
